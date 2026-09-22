8e91aad6c0548a2509e76bf90a3181457f73813c
selace <eadagla@byupathway.edu>
2026-09-22 21:01:12 +0000

    Add cart item count badge

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,37 @@
 import ProductData from "./ProductData.mjs";
+
 import ProductList from "./ProductList.mjs";
+
 import { initSearchForm } from "./search-form.mjs";
-import { loadHeaderFooter } from "./utils.mjs";
+
+import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
+
+function updateCartCount() {
+  const cartItems = getLocalStorage("so-cart") || [];
+
+  const totalItems = cartItems.reduce(
+    (total, item) => total + (item.Quantity || 1),
+    0,
+  );
+
+  const cartCount = document.querySelector(".cart-count");
+
+  if (cartCount) {
+    cartCount.textContent = totalItems;
+  }
+}
 
 async function initApp() {
-  await loadHeaderFooter(); //to prevent search-form null
+  await loadHeaderFooter();
+
+  updateCartCount();
 
   const dataSource = new ProductData("tents");
 
   const element = document.querySelector(".product-list");
 
   const productList = new ProductList("Tents", dataSource, element);
+
   productList.init();
 
   initSearchForm(document.querySelector(".search-form"));
